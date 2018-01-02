31e48b79183dc521a01e29f998b6db3616bec22b
wanghuaping <wanghp_8609@163.com>
2018-01-02 11:14:18 +0800

    new class

--- src/main/java/com/lanysec/idrecoginition/Test.java
+++ src/main/java/com/lanysec/idrecoginition/Test.java
@@ -2,14 +2,19 @@ package com.lanysec.idrecoginition;
 
 import net.sourceforge.tess4j.Tesseract;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 
     public static String test(String path){
         File imageFile = new File(path);
         Tesseract instance = new Tesseract();
-        instance.setDatapath("D:\\work\\idre\\data"); //设置训练库的位置
+        instance.setDatapath("/usr/local/share/tessdata"); //设置训练库的位置
         instance.setLanguage("eng");
         try {
             String result = instance.doOCR(imageFile);
@@ -21,7 +26,65 @@ public class Test {
         return "";
     }
 
+    private final String LANG_OPTION = "0";
+    private final String EOL = System.getProperty("line.sperator");
+    private String tessPath = new File("tesseract").getAbsolutePath();
+
+    public String recognizeText(File imageFile) throws Exception{
+        File outputFile = new File(imageFile.getParentFile(),"output");
+        StringBuffer str8 = new StringBuffer();
+        List<String> cmd = new ArrayList<String>();
+        String os = System.getProperty("os.name");
+        if(os.toLowerCase().startsWith("win")){
+            cmd.add("tesseract");
+        }else{
+            cmd.add("tesseract");
+        }
+        cmd.add(imageFile.getName());
+        cmd.add(outputFile.getName());
+        cmd.add("digits");
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.directory(imageFile.getParentFile());
+        pb.command(cmd);
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+        int w = process.waitFor();
+        if(w == 0){
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(outputFile.getAbsolutePath() + ".txt"),"UTF-8"));
+            String str;
+            while((str = in.readLine()) != null){
+                str8.append(str).append(EOL);
+            }
+            in.close();
+        }else{
+            String msg;
+            switch(w){
+                case 1:
+                    msg = "Errors accessing files. There may be spaces in your image's filename.";
+                    break;
+                case 29:
+                    msg = "Cannot recognize the image or its selected region.";
+                    break;
+                case 31:
+                    msg = msg = "Unsupported image format.";
+                    break;
+                default:
+                    msg = "Errors occurred.";
+            }
+            throw new RuntimeException(msg);
+        }
+        new File(outputFile.getAbsolutePath() + ".txt").delete();
+        return str8.toString().replaceAll("\\s*","");
+    }
+
     public static void main(String[] args){
-        Test.test("D:\\work\\idre\\img_src\\id_card.jpg");
+        //Test.test("/Users/wanghuaping/Documents/idre/id.jpg");
+        Test test = new Test();
+        try{
+            String str = test.recognizeText(new File("/Users/wanghuaping/Documents/idre/id.jpg"));
+            System.out.println(str);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
